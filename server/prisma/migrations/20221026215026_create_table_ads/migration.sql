1e3933fd010eada6005a247a19fb8000bd93711b
Matheus Magnani <mrmatheusmagnani@gmail.com>
2023-01-26 17:37:04 -0300

    Update app

---
+++ server/prisma/migrations/20221026215026_create_table_ads/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Ad" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "yearsPlaying" INTEGER NOT NULL,
+    "discord" TEXT NOT NULL,
+    "weekDays" TEXT NOT NULL,
+    "hourStart" INTEGER NOT NULL,
+    "hourEnd" INTEGER NOT NULL,
+    "useVoiceChannel" BOOLEAN NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
